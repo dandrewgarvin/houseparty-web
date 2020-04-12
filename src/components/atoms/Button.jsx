4d75869b34d515ddc38e855183d609de6e5cc4cd
Andrew Garvin <me@andrewgarv.in>
2020-04-12 13:04:23 -0600

    add button atom

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,20 @@
+/**
+ * @author Andrew Garvin
+ * @timestamp April 12, 2020
+ * @copyright ControlBit Studios 2020. All rights reserved.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button() {
+  return (
+    <section id="Button">
+      <h1>Button Atom</h1>
+    </section>
+  );
+}
+
+Button.propTypes = {};
+
+export default Button;
